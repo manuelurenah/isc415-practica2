b142b84523e2eecede4cf9aa752c8f6de46f01fd
Manuel E. Ureña <manuel.ureh@hotmail.com>
2016-07-10 20:17:35 -0400

    Add Procfile to deploy on heroku

--- src/main/java/edu/pucmm/Main.java
+++ src/main/java/edu/pucmm/Main.java
@@ -2,7 +2,7 @@ package edu.pucmm;
 
 import edu.pucmm.handlers.TemplateHandler;
 import spark.Spark;
-
+import spark.Spark.*;
 /**
  * Created by MEUrena on 5/21/16.
  * All rights reserved.
@@ -10,6 +10,16 @@ import spark.Spark;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        Spark.port(port);
 
         Spark.staticFileLocation("/public");
 
