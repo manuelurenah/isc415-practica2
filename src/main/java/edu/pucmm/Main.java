9e742c3b28f7a900f9d225d12c30ada799e808c7
Manuel E. Ureña <manuel.ureh@hotmail.com>
2016-07-15 14:53:02 -0400

    Make changes to deploy on Heroku

--- src/main/java/edu/pucmm/Main.java
+++ src/main/java/edu/pucmm/Main.java
@@ -2,7 +2,11 @@ package edu.pucmm;
 
 import edu.pucmm.handlers.TemplateHandler;
 import spark.Spark;
+import org.h2.tools.Server;
 import spark.Spark.*;
+
+import java.sql.SQLException;
+
 /**
  * Created by MEUrena on 5/21/16.
  * All rights reserved.
@@ -22,6 +26,14 @@ public class Main {
         Spark.port(port);
 
         Spark.staticFileLocation("/public");
+
+        Server server = null;
+        try {
+            server = Server.createTcpServer("-tcpAllowOthers").start();
+        } catch (SQLException e) {
+            System.out.println("There was an error opening the database:");
+            e.printStackTrace();
+        }
 
         new TemplateHandler().startApp();
 
